d55c9f18ec2f5992a7e4221152a65265a1dbd60c
Charles Glass <charlie-glass@live.com>
2019-10-02 13:59:19 -0700

    added number question

--- app.js
+++ app.js
@@ -6,12 +6,12 @@ var booleon = true;
 
 var userName = prompt('What\'s your name?');
 // console.log('userName: '+ userName);
-alert('Hello '+userName+', welcome to my about me page, I am going to ask you 5 questions to see how well you know me!');
+alert('Hello ' + userName + ', welcome to my about me page, I am going to ask you 5 questions to see how well you know me!');
 
 var bornInWashington = prompt('Question 1: Was I born in Washington?');
 // console.log('bornInWashington: '+ bornInWashington);
 bornInWashington = bornInWashington.toLowerCase();
-if(bornInWashington === 'yes' || bornInWashington === 'y') {
+if (bornInWashington === 'yes' || bornInWashington === 'y') {
   alert('Correct, I was born in Woodinville, Washington!');
 }
 else {
@@ -21,7 +21,7 @@ else {
 var leftHanded = prompt('Am I left handed?');
 // console.log('leftHanded: '+leftHanded);
 leftHanded = leftHanded.toLowerCase();
-if(leftHanded === 'no' || leftHanded === 'n') {
+if (leftHanded === 'no' || leftHanded === 'n') {
   alert('Correct, I am not left handed, except when it comes to shooting pool');
 }
 else {
@@ -31,7 +31,7 @@ else {
 var canadian = prompt('Have I ever lived outside of Washington?');
 // console.log('canadian: '+canadian);
 canadian = canadian.toLowerCase();
-if(canadian === 'yes' || canadian === 'y') {
+if (canadian === 'yes' || canadian === 'y') {
   alert('Correct, I lived in Canada for awhile when I was younger');
 }
 else {
@@ -41,22 +41,52 @@ else {
 var tattoo = prompt('Do I have a tattoo?');
 // console.log('tattoo: '+tattoo);
 tattoo = tattoo.toLowerCase();
-if(tattoo === 'yes' || tattoo === 'n') {
+if (tattoo === 'yes' || tattoo === 'n') {
   alert('Correct, I have a charmanader tattoo on my left shoulder');
 }
-else{
+else {
   alert('Incorrect, I have a charmander tattoo on my left shoulder');
 }
 
 var codingBackground = prompt('Do I have a background in coding?');
 // console.log('codingBackground '+codingBackground);
 codingBackground = codingBackground.toLowerCase();
-if(codingBackground === 'no' || codingBackground === 'n') {
+if (codingBackground === 'no' || codingBackground === 'n') {
   alert('Correct, I do not have a background in coding');
 }
-else{
+else {
   alert('Incorrect, I do not have a background in coding');
 }
 
 
-alert('Thanks for taking my short about me quiz, '+userName+'!');
+alert('Thanks for taking my short about me quiz, ' + userName + '!');
+
+
+
+
+var tries = 4;
+var answer = 3;
+
+
+while (tries > 0) {
+  var guess = prompt('What is my favorite number between 1 and 10');
+  console.log('guess: ' + guess);
+  if (guess > answer) {
+    alert('Your guess is too high.');
+  }
+  else if (guess < answer) {
+    alert('Your guess is too low');
+  }
+
+  else {
+    alert('Great job, my favorite number is 3!');
+    { break; }
+  }
+  tries = tries - 1;
+  while(tries === 0) {
+    alert('You ran out of tries, my favorite number is 3');
+    { break; }
+  }
+}
+
+alert('Thanks for taking my short about me quiz, ' + userName + '!');
